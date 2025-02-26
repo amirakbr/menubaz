8926e0f3368a0121c662d99998c671ac176e53f4
amirakbr <amir.akbrzadh3@gmail.com>
2025-02-26 18:53:44 +0330

    refactor

--- src/app/_components/contactUs/_components/socialMediaContact.tsx
+++ src/app/_components/contactUs/_components/socialMediaContact.tsx
@@ -1,29 +1,35 @@
-import Message2Icon from '@/app/public/icons/message-2';
-import Link from 'next/link';
+import Message2Icon from "@/app/public/icons/message-2";
+import Link from "next/link";
 
 const SocialMediaContact = () => {
   return (
     <div className="order-2 xl:order-1 xl:w-[368px] xl:h-[276px] flex flex-col gap-4 items-center justify-center bg-white-200 rounded-lg p-4">
       <Message2Icon />
       <p className="text-title text-base">با افتخار، پاسخگوی شما هستیم</p>
-      <div className="flex items-center gap-4">
-        <p className="text-title-400 text-sm">پاسخگوی تلگرام</p>
-        <Link className="text-base text-complementary" href={'mailto:support@menobuzz.com'}>
+      <div className="flex items-center gap-4 font-medium text-xs xl:text-sm">
+        <p className="text-title-400">پاسخگوی تلگرام</p>
+        <Link
+          className="text-base text-complementary"
+          href={"mailto:support@menobuzz.com"}
+        >
           support@menobuzz.com
         </Link>
       </div>
-      <div className="flex items-center gap-4">
-        <p className="text-title-400 text-sm">پاسخگوی واتساپ</p>
-        <Link className="text-base text-complementary" href={'mailto:support@menobuzz.com'}>
+      <div className="flex items-center gap-4 font-medium text-xs xl:text-sm">
+        <p className="text-title-400">پاسخگوی واتساپ</p>
+        <Link
+          className="text-base text-complementary"
+          href={"mailto:support@menobuzz.com"}
+        >
           support@menobuzz.com
         </Link>
       </div>
       <p className="text-center text-title-400 text-xs">
-        تفاوت را با منوباز تجربه کنید. تیم پشتیبانی اختصاصی ما اینجاست تا به شما 24 ساعته کمک کند و از تجربه ایمیلی روان و بدون دردسر اطمینان حاصل
-        کند.
+        تفاوت را با منوباز تجربه کنید. تیم پشتیبانی اختصاصی ما اینجاست تا به شما
+        24 ساعته کمک کند و از تجربه ایمیلی روان و بدون دردسر اطمینان حاصل کند.
       </p>
     </div>
   );
 };
 
-export default SocialMediaContact
+export default SocialMediaContact;
