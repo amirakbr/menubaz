e05b146b2e5ea6e5023d4338f2561e83c30150b3
amirakbr <amir.akbrzadh3@gmail.com>
2025-02-26 19:54:41 +0330

    refactor = refactor not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,15 +5,19 @@ import LeftChevronicon from "./public/icons/leftChevronIcon";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full my-20">
+    <div className="flex flex-col items-center justify-center h-full my-20 px-4">
       <h1>صفحه ای که دنبال آن بودید یافت نشد!!!</h1>
       <NotFoundIcon height={200} />
-      <Button variant="secondary" size="small">
-        <Link className="flex items-center gap-4" href="/">
+      <Link href="/">
+        <Button
+          className="flex items-center gap-4"
+          variant="secondary"
+          size="small"
+        >
           صفحه اصلی
           <LeftChevronicon />
-        </Link>
-      </Button>
+        </Button>
+      </Link>
     </div>
   );
 };
