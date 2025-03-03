df44c9bfb6382a02319fafd032f46ca4b6e02f97
amir akbarzadeh <amir.akbrzadh3@gmail.com>
2025-03-04 01:26:22 +0330

    tsx : add our values

---
+++ src/app/aboutus/_components/ourValues.tsx
@@ -0,0 +1,9 @@
+const OurValues = () => {
+     return (
+          <div className="flex flex-col gap-7">
+               <p className="text-title text-2xl font-bold">
+                    ارزش های ما
+               </p>
+          </div>
+     )
+}
